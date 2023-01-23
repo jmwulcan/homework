b645006af697e9a003d6809d00b952d0c13527a5
jmwulcan <jmwulcan@gmail.com>
2023-01-23 07:19:35 -0800

    completed

--- 23fizzbuzz.py
+++ 23fizzbuzz.py
@@ -7,6 +7,12 @@
 
 # Note: this is a common interview question
 
+for i in range (1, 100):
+	if i % 5 == 0 and i % 3 == 0: print('FizzBuzz')
+	elif i % 5 == 0: print('Buzz')
+	elif i % 3 == 0: print('Fizz')
+	else: print(i)
+
 
 """
 python3 23fizzbuzz.py
