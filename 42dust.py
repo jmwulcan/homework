085e0542254f301039873f0319b22a95c8224fde
jmwulcan <116297268+jmwulcan@users.noreply.github.com>
2023-03-13 17:16:37 -0700

    fixed

--- 42dust.py
+++ 42dust.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # 42dust.py
 
 # Write a program that performs entropy filtering on nucleotide fasta files
@@ -17,62 +19,48 @@ import mcb185
 import math
 
 #function for entropy filtering
+def entropy_filter(seq, w):
 
-def entropy_filter(file, w, threshold):
+	A = 0
+	T = 0
+	G = 0
+	C = 0
 	
-	for defline, seq in mcb185.read_fasta(file):
-		
-		newseq = ''
-		for pos in range(len(seq) - w + 1):
-
-			subseq = seq[pos:pos+w]
-			
-			A = 0
-			T = 0
-			G = 0
-			C = 0
-			
-			for nt in subseq:
-				if nt == 'A':
-					A += 1
-				elif nt == 'T':
-					T += 1
-				elif nt == 'G':
-					G += 1
-				elif nt == 'C':
-					C += 1
-			
-			pA = A / w
-			pT = T / w
-			pG = G / w
-			pC = C / w
-			
-			pNTs = [pA, pT, pG, pC]
-			
-			H = 0
-			for pNT in pNTs:
-				if pNT != 0:
-					H += -(pNT * math.log2(pNT))
-			
-			#print(H)
-			
-			if H < threshold:
-				newseq += 'N'
-			else: 
-				newseq += subseq[0]
-	return defline, newseq
-		
+	for nt in seq:
+		if nt == 'A': A += 1
+		if nt == 'T': T += 1
+		if nt == 'G': G += 1
+		if nt == 'C': C += 1
+	
+	pA = A / w
+	pT = T / w
+	pG = G / w
+	pC = C / w
+	
+	pNTs = [pA, pT, pG, pC]
+	
+	H = 0
+	for pNT in pNTs:
+		if pNT !=0:
+			H += -(pNT * math.log2(pNT))
+	
+	return H
 
 file = sys.argv[1]
 w = int(sys.argv[2])
-threshold = float(sys.argv[3])	
+t = float(sys.argv[3])
 
-defline, newseq = entropy_filter(file, w, threshold)
-
-
-print(defline)
-for pos in range(0, len(newseq), 60):
-	print(newseq[pos:pos+60])
+for defline, seq in mcb185.read_fasta(file):
+	newseq = ''
+	for i in range (len(seq)-w+1):
+		wseq = seq[i: i+w]
+		H = entropy_filter(wseq, w)
+		if H < t: newseq += 'N'
+		else: newseq += wseq[0]
+	print('>', end='')
+	print(defline)
+	for pos in range(0, len(newseq), 60):
+		print(newseq[pos: pos+60])
 
 """
 python3 42dust.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_genomic.fna.gz 11 1.4
